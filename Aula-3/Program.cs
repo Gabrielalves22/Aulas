25ebbd191408e64b104a33c4bd71e232513812ba
Gabriel Alves <gabriel_alves2207@outlook.com>
2026-05-05 14:55:37 -0300

    Aula-3 Finalizada

--- Aula-3/Program.cs
+++ Aula-3/Program.cs
@@ -4,6 +4,35 @@ public class program
 {
     public static void Main()
     {
-        
+        /* Execicio de variavel
+        string name = "gabriel";
+
+        name = "Paulo";
+
+        Console.WriteLine ("A variávem tem como valor antigo: " + name);
+
+        name = "Alana";
+        Console.WriteLine ("A variávem tem como valor novo: " + name);
+        */
+
+        // constante
+
+        // const
+        const int number = 1000;
+
+        Console.WriteLine ("O valor da constante é : " + number);
+
+        // constante type var
+        var variable = "gabriel";
+        //variable = 200;
+
+        Console.WriteLine ("O valor da varíavel é : " + variable);
+
+        // cosntante type dynamic
+
+        dynamic variable2 = 20;
+        variable2 = "Gabriel";
+
+        Console.WriteLine ("O valor da Variável é : " + variable2);
     }
 }
