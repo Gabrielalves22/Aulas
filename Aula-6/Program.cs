6ddbbb6fa8b8b5f28bb69b1a4d0a01e26b0c9f7d
Gabriel Alves <gabriel_alves2207@outlook.com>
2026-05-05 18:56:41 -0300

    Finalizada Aula-6 (if e else)

--- Aula-6/Program.cs
+++ Aula-6/Program.cs
@@ -5,5 +5,23 @@ public class program
     public static void Main()
     {
         // if e else
+        Console.WriteLine ("====== Verificador de numeros inpares e pares ======");
+
+        Console.WriteLine ("Digite um numero: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int result = number % 2;
+
+        if (result == 0)
+        {
+            Console.WriteLine ("O numero é par!");
+        }
+        else
+        {
+            Console.WriteLine ("O numero é impar!");
+        }
+        
+
+        Console.WriteLine ("====================================================");
     }
 }
