b8c2769aa9b370fe95d836718d8843ce09ad7388
Gabriel Alves <gabriel_alves2207@outlook.com>
2026-05-05 15:48:12 -0300

    Finalizado Aula-4

--- Aula-4/Program.cs
+++ Aula-4/Program.cs
@@ -4,6 +4,21 @@ public class program
 {
     public static void Main()
     {
-        
+        // inoput e outinput
+        Console.WriteLine ("--------- CADASTRO DE USUÁRIOS --------");
+
+        //solicitar o nome do usuário
+        Console.WriteLine ("Digite seu nome: ");
+        string name = Console.ReadLine();
+
+        //solicitação da idade do usuário
+        Console.WriteLine ("Digite sua Idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine ("---------------------------------------");
+
+        Console.WriteLine ("O nome de usuário é: " + name);
+        Console.WriteLine ("A idade do usuário é: " + age);
+
     }
 }
