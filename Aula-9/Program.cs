7c65116e50a066b2e1c7f0a1b5eacaab0109d251
Gabriel Alves <gabriel_alves2207@outlook.com>
2026-05-07 09:46:58 -0300

    Aula-9 Finalizada (Operadores Logicos)

--- Aula-9/Program.cs
+++ Aula-9/Program.cs
@@ -4,6 +4,46 @@ public class program
 {
     public static void Main()
     {
+        Console.WriteLine ("===== Oeradores Lógicos =====");
+
+        bool isloggedIn = true;
+        bool hasAminAcces = false;
+
+        Console.WriteLine ("\ninformaçoes do usuario:");
+        Console.WriteLine ("Usuario logado: " + isloggedIn);
+        Console.WriteLine ("Acesso ao painel de administradores: " + hasAminAcces);
+
+        Console.WriteLine ("\nPermissões");
         
+        // Operador (||) - ou
+        if (isloggedIn || hasAminAcces)
+        {
+            Console.WriteLine ("Acesso ao sistema Concedido!");
+        }
+        else
+        {
+            Console.WriteLine ("Acesso ao sistema Negado!");
+        }
+
+        // Operador (&&) - E
+        if (isloggedIn && hasAminAcces)
+        {
+            Console.WriteLine ("Painel de Adimin Liberado!");
+        }
+        else
+        {
+            Console.WriteLine ("Acesso ao painel de administradores Negado!");
+        }
+
+        // Operador (!) - Negação
+        if (!isloggedIn)
+        {
+            Console.WriteLine ("Usuário não está logado");
+        }
+        else
+        {
+            Console.WriteLine ("Usuário está logado");
+        }
+
     }
 }
